e423d52ce1ef0d18ff0da0203bef79d1730f2eb5
Mohammed-Imran-Khan <127581145+Mohammed-Imran-Khan@users.noreply.github.com>
2023-11-18 18:24:08 +0530

    Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
     const navigate = useNavigate();
     const handleLogin = async (e)=>{
         e.preventDefault();
-        const response =  await fetch("https://noteapp-server-3umv.onrender.com//api/auth/login", {
+        const response =  await fetch("https://noteapp-server-3umv.onrender.com/api/auth/login", {
             method: "POST",
             headers:{
                 "Content-Type":"application/json"
